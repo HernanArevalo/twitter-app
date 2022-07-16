2848633bd0e30144414902cc074dfad0ba351696
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-07-15 21:42:55 -0300

    navbar responsive items

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,16 +1,30 @@
 
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState, useEffect } from 'react'
+
 import { Link, NavLink, useLocation } from 'react-router-dom';
 
 import './Navbar.css'
 
 export const Navbar = () => {
 
-    const { pathname } = useLocation()
+    const { pathname } = useLocation();
+
+    const navBar = useRef()
+
+    const [navBarWidth, setNavBarWidth] = useState(200)
+
+    useLayoutEffect(()=> {
+
+        const navBarWidth =  navBar.current.getBoundingClientRect().width 
+
+        setNavBarWidth(navBarWidth)
+
+
+    } )
 
   return (
     <>
-    <nav className="navbar-container">
+    <nav className="navbar-container" ref={ navBar }>
 
         <div className='navbar-items'>
             <Link to="/home">
@@ -19,157 +33,165 @@ export const Navbar = () => {
                 </div>
             </Link>
 
+            <div>
+                <NavLink 
+                    to="home"
+                    className={({isActive}) =>
+                        isActive? 'active': 'inactive' }
+                    >
+                    <div className="inicioNav leftItem">
+                        <div>
+                            
+                            {
+                                pathname.includes('/home')
+                                ?
+                                <i className='bx bxs-home-circle'></i>
+                                :
+                                <i className='bx bx-home-circle'></i>
+                            }
 
-            <NavLink 
-                to="home"
-                className={({isActive}) =>
-                    isActive? 'active': 'inactive' }
+                            {
+                                navBarWidth > 250
+                                ?
+                                <span className="leftItemText">Inicio</span>
+
+                                :
+                                null
+                            }
+
+                        </div>
+                    </div>
+                </NavLink>
+                <NavLink 
+                    to="explore"
+                    className={({isActive}) =>
+                        isActive? 'active': '' }
                 >
-                <div className="inicioNav leftItem">
-                    <div>
-                        
-                        {
-                            pathname.includes('/home')
-                            ?
-                            <i className='bx bxs-home-circle'></i>
-                            :
-                            <i className='bx bx-home-circle'></i>
-                        }
+                    <div className="inicioNav leftItem">
+                        <div>
 
-                        <span className="leftItemText">Inicio</span>
+                            <i className='bx bx-hash'></i>
+                            <span className="leftItemText">Explorar</span>
+
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="explore"
-                className={({isActive}) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
+                </NavLink>
+                <NavLink 
+                    to="notifications"
+                    className={({isActive}) =>
+                        isActive? 'active': '' }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
+                            {
+                                pathname.includes('/notifications')
+                                ?
+                                <i className='bx bxs-bell'></i>
+                                :
+                                <i className='bx bx-bell'></i>
+                            }
 
-                        <i className='bx bx-hash'></i>
-                        <span className="leftItemText">Explorar</span>
+                            <span className="leftItemText">Notificaciones</span>
 
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="notifications"
-                className={({isActive}) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
-                        {
-                            pathname.includes('/notifications')
-                            ?
-                            <i className='bx bxs-bell'></i>
-                            :
-                            <i className='bx bx-bell'></i>
-                        }
+                </NavLink>
+                <NavLink 
+                    to="messages"
+                    className={ ( {isActive} ) =>
+                        isActive? 'active': '' }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
+                            {
+                                pathname.includes('/messages')
+                                ?
+                                <i className='bx bxs-envelope'></i>
+                                :
+                                <i className='bx bx-envelope'></i>
+                            }
 
-                        <span className="leftItemText">Notificaciones</span>
+                            <span className="leftItemText">Mensajes</span>
 
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="messages"
-                className={ ( {isActive} ) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
-                        {
-                            pathname.includes('/messages')
-                            ?
-                            <i className='bx bxs-envelope'></i>
-                            :
-                            <i className='bx bx-envelope'></i>
-                        }
+                </NavLink>
+                <NavLink 
+                    to="saves"
+                    className={({isActive}) =>
+                        isActive? 'active': '' }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
 
-                        <span className="leftItemText">Mensajes</span>
+                            {
+                                pathname.includes('/saves')
+                                ?
+                                <i className='bx bxs-bookmark'></i>
+                                :
+                                <i className='bx bx-bookmark'></i>
+                            }
 
+                            <span className="leftItemText">Guardados</span>
+
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="saves"
-                className={({isActive}) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
+                </NavLink>
+                <NavLink 
+                    to="lists"
+                    className={({isActive}) =>
+                        isActive? 'active': '' }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
 
-                        {
-                            pathname.includes('/saves')
-                            ?
-                            <i className='bx bxs-bookmark'></i>
-                            :
-                            <i className='bx bx-bookmark'></i>
-                        }
+                            {
+                                pathname.includes('/lists')
+                                ?
+                                <i className='bx bxs-detail'></i>
+                                :
+                                <i className='bx bx-detail'></i>
+                            }
 
-                        <span className="leftItemText">Guardados</span>
-
+                            <span className="leftItemText">Listas</span>
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="lists"
-                className={({isActive}) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
+                </NavLink>
+                <NavLink 
+                    to="profile"
+                    className={({isActive}) =>
+                        isActive? 'active': '' }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
 
-                        {
-                            pathname.includes('/lists')
-                            ?
-                            <i className='bx bxs-detail'></i>
-                            :
-                            <i className='bx bx-detail'></i>
-                        }
+                            {
+                                pathname.includes('/profile')
+                                ?
+                                <i className='bx bxs-user'></i>
+                                :
+                                <i className='bx bx-user'></i>
+                            }
 
-                        <span className="leftItemText">Listas</span>
+                            <span className="leftItemText">Perfil</span>
+
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to="profile"
-                className={({isActive}) =>
-                    isActive? 'active': '' }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
-
-                        {
-                            pathname.includes('/profile')
-                            ?
-                            <i className='bx bxs-user'></i>
-                            :
-                            <i className='bx bx-user'></i>
-                        }
-
-                        <span className="leftItemText">Perfil</span>
-
+                </NavLink>
+                <NavLink 
+                    to=""
+                    className={({isActive}) =>
+                        isActive? 'active': ''
+                }
+                >
+                    <div className="inicioNav leftItem">
+                        <div>
+                            <i className='bx bx-dots-horizontal-rounded'></i>
+                            <span className="leftItemText">Más opciones</span>
+                        </div>
                     </div>
-                </div>
-            </NavLink>
-            <NavLink 
-                to=""
-                className={({isActive}) =>
-                    isActive? 'active': ''
-            }
-            >
-                <div className="inicioNav leftItem">
-                    <div>
-                        <i className='bx bx-dots-horizontal-rounded'></i>
-                        <span className="leftItemText">Más opciones</span>
-                    </div>
-                </div>
-            </NavLink>
-
+                </NavLink>
+            </div>
 
         </div>
 
